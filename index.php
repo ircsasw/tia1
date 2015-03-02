81a1d51078809c70852f508e06e97cb1fd51a6ea
BenjaminCriollo <benjamincriollo@gmail.com>
2015-03-02 07:55:04 -0600

    html

--- index.php
+++ index.php
@@ -3,3 +3,27 @@
 	echo "Equipo 1";
 
 ?>
+
+<html>
+	<head>
+		<title>mi blog</title>
+	</head>
+	<body>
+		<table border="1">
+			<tr>
+				<th>alumnos</th>
+				<th>comentarios</th>
+				<th>temas</th>
+				<th>usuarios</th>
+			</tr>
+				<?php
+					while ($row = mysql_fetch_array($rs_alumnos)) 
+					{
+						echo "<tr>";
+						echo "<td>". $row ["alumnos"]. "</td>";
+						echo"<td>". $row ["comentarios"]. "</td>";
+						echo "<td>". $row ["temas"]. 	}
+			
+		</table>
+	</body>
+</html>
