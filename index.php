27c403fd33b886b09ca67f2e6e69afc3708cbb2d
BenjaminCriollo <benjamincriollo@gmail.com>
2015-03-06 08:00:59 -0600

    html

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $Link) or die(mysql_error());
 					{
 						echo "<tr>";
 						echo "<td>". $row ["id"]. "</td>";
-						echo"<td>". $row ["id_usuario"]. "</td>";
+						echo "<td>". $row ["id_usuario"]. "</td>";
 						echo "<td>". $row ["titulo"]."</td>";
 						echo "<td>". $row ["fecha_pub"]. "</td>";
 						echo "<td>". $row ["contenido"]. "</td>";
@@ -44,6 +44,27 @@ $Link) or die(mysql_error());
 
 
 			
+		</table>
+		<table border="1">
+			<tr>
+			 <td colspan="3"> titulo1 </td> <td> controles</td>
+			</tr>
+			<tr>
+				<td colspan="4"> usuario y fecha</td>
+			</tr>
+			<tr>
+				<td colspan="4">comentario</td>
+			</tr>
+			<tr>
+				<td colspan="4">me gusta </td> 
+			</tr>
+			<tr>
+				<td colspan="3"> usuario y fecha </td> <td> controles</td>
+			</tr>
+			<tr> 
+				<td colspan="4"></td>
+			</tr>
+
 		</table>
 	</body>
 </html>
