adac3a49cae532f70cb4c6539b67ebd2e41514d2
unknown <aniversario1>
2015-03-10 08:38:07 -0600

    login de nuevo

--- index.php
+++ index.php
@@ -19,11 +19,7 @@ $Link) or die(mysql_error());
 	<head>
 		<title>mi blog</title>
 	</head>
-	<body> 
-	<?php ($row = mysql_fetch_array($rs_temas)); 
-	echo "bienvenidos a mi blog " . $row["id_usuario"]; 
-	 ?>
-	 <!--
+	<body>
 		<table border="1">
 			<tr>
 				<th>ID</th>
@@ -50,14 +46,13 @@ $Link) or die(mysql_error());
 <head>
 	<title>mi blog</title>
 </head>
-<body>
-	<a href="longin.php"> longin </a>
-</body>
 </html>
 
-			-->
-		</table>
+<body>
 
+			
+		<a href="longin.php"> longin </a>
+		</table>
 		
 		<table border="1">
 		<?php ($row = mysql_fetch_array($rs_temas)) ?>
@@ -82,5 +77,4 @@ $Link) or die(mysql_error());
 
 		</table>
 	</body>
-
 </html>
