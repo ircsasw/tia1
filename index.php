31d5759aa9ade64f2358cb10488d781b2701a34c
Arturo <ircsasw@gmail.com>
2015-03-11 08:50:42 -0500

    Acceso de boton

--- index.php
+++ index.php
@@ -24,12 +24,12 @@ $Link) or die(mysql_error());
 		<?php ($row = mysql_fetch_array($rs_temas));
 			echo "<h1> Bienvenidos a mi blog" . $row["id_usuario"] ."</h1>";
 			?>
-		<table border="1"> 
+		<table border="1">
 
 		<table border="1">
 
 				<?php /**
-					while ($row = mysql_fetch_array($rs_temas)) 
+					while ($row = mysql_fetch_array($rs_temas))
 					{
 						echo "<tr>";
 						echo "<td>". $row ["id"]. "</td>";
@@ -40,16 +40,21 @@ $Link) or die(mysql_error());
 						echo "</tr>";
 
 					}
-				**/ ?> 
+				**/ ?>
 
-			
+
 		<a href="longin.php"> longin </a><br>
 		</table>
-		
+
 		<table border="1">
-		
+
 			<tr>
-			 <td colspan="3"> <?php echo $row["titulo"]; ?>  </td> <td> <input type="submit" name="comentar" value="comentar"> <input type="submit" name="editar" value="editar"> <input type="submit" name="borrar" value="borrar"></td>
+			 <td colspan="3"> <?php echo $row["titulo"]; ?>  </td>
+			 <td>
+			 	<input type="submit" name="comentar" value="comentar" onclick="window.location.href='comentar.php'">
+			 	<input type="submit" name="editar" value="editar">
+			 	<input type="submit" name="borrar" value="borrar">
+			 </td>
 			</tr>
 			<tr>
 				<td colspan="4"> <?php echo $row["id_usuario"] . "-" . $row["fecha_pub"]; ?></td>
@@ -58,12 +63,12 @@ $Link) or die(mysql_error());
 				<td colspan="4"><?php echo $row["contenido"]; ?></td>
 			</tr>
 			<tr>
-				<td colspan="4"> <input type="submit" name="Me gusta" value="Me gusta"> </td> 
+				<td colspan="4"> <input type="submit" name="Me gusta" value="Me gusta"> </td>
 			</tr>
 			<tr>
 				<td colspan="3"> <?php echo $row["id_usuario"] . "-" . $row["fecha_pub"]; ?></td> <td> <input type="submit" name="editar" value="editar"> <input type="submit" name="borrar" value="borrar"> </td>
 			</tr>
-			<tr> 
+			<tr>
 				<td colspan="4"> comentario</td>
 			</tr>
 
